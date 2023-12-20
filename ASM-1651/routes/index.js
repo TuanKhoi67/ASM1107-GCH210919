09ad4d92e6e44209418262648379ccfa0d61f3d1
TKhoi2 <153786779+TuanKhoi67@users.noreply.github.com>
2023-12-20 20:52:01 +0700

    update

--- ASM-1651/routes/index.js
+++ ASM-1651/routes/index.js
@@ -12,7 +12,9 @@ router.get('/', async (req, res) => {
 
 router.post('/search', async (req, res) => {
   var keyword = req.body.keyword;
-  var toys = await ToyModel.find({ name: new RegExp(keyword , "i") });
+  //SQL: SELECT * FROM toys WHERE model LIKE '%keyword%'
+  var toys = await ToysModel.find({ name: new RegExp(keyword, "i") }).populate('brand');
   res.render('index', { toys })
-})
+}) 
+
 module.exports = router;
